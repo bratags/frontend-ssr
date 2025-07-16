69cfe45bd75f1f9f2563818ad9eb24c9f0a2f9b4
Will <will@onezerohosting.com>
2025-07-15 18:43:53 -0700

    .

--- pages/bratag/+onBeforePrerenderStart.js
+++ pages/bratag/+onBeforePrerenderStart.js
@@ -1,9 +1,9 @@
 const {
-    MODE,
     VITE_APP_API_KEY
-} = import.meta.env
+} = process.env
 
 const onBeforePrerenderStart = async () => {
+    const MODE = process.env.NODE_ENV === 'production' ? 'production' : 'development'
     const url = MODE === 'production' ? 'https://api.june07.com/v1/bratags/ids' : 'https://api.dev.june07.com/v1/bratags/ids'
     const { ids, cursor } = await fetch(url, {
         headers: {

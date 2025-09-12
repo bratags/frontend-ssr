7235941ab04c99c8ed934089ba2e8abac48aa8cc
Will <will@onezerohosting.com>
2025-09-12 16:32:25 -0700

    .

--- pages/donate/+onRenderHtml.js
+++ pages/donate/+onRenderHtml.js
@@ -1,7 +1,15 @@
 import donateHtml from './index.html?raw'
 
 export function onRenderHtml() {
-    return {
-        documentHtml: donateHtml
+    let html = donateHtml
+
+    if (import.meta.env.PROD) {
+        // inject the refresh meta right after <head>
+        html = html.replace(
+            /<head>/,
+            `<head>\n<meta http-equiv="refresh" content="0;url=https://bratags.com/?donate=1">`
+        )
     }
+
+    return { documentHtml: html }
 }

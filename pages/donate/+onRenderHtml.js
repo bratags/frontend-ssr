d2c5709c80f1263c94511e6c8fe59290ebb38fa6
Will <will@onezerohosting.com>
2025-09-12 15:38:04 -0700

    .

--- pages/donate/+onRenderHtml.js
+++ pages/donate/+onRenderHtml.js
@@ -1,8 +1,12 @@
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export function onRenderHtml() {
-    const htmlFilePath = path.join(process.cwd(), 'index.html')
+    const htmlFilePath = path.join(__dirname, 'index.html')
     const documentHtml = fs.readFileSync(htmlFilePath, 'utf-8')
 
     return { documentHtml }
